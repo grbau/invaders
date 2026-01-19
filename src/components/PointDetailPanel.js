e9935b5544bf3646de280cb80f9025915a834343
baudic <gregory.baudic@etam.fr>
2026-01-19 15:36:28 +0100

    fix insta link

--- src/components/PointDetailPanel.js
+++ src/components/PointDetailPanel.js
@@ -426,7 +426,7 @@ export default function PointDetailPanel({ point, onClose }) {
                   </a>
                   {form.name && (
                     <a
-                      href={`https://www.instagram.com/explore/tags/${form.name.toUpperCase().replace(/[^A-Z0-9_]/g, '').replace(/_0+(\d)/g, '_0$1')}/`}
+                      href={`https://www.instagram.com/explore/tags/${form.name.toUpperCase().replace(/[^A-Z0-9_]/g, '').replace(/_0*(\d+)/, (_, num) => '_' + (num.length === 1 ? '0' + num : num))}/`}
                       target="_blank"
                       rel="noopener noreferrer"
                       className="inline-flex items-center gap-1 text-pink-500 hover:text-pink-700 text-sm"
